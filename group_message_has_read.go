8753f14bdc08d9ddaf5e90283cbffd12116c4061
Gordon <1432970085@qq.com>
2022-03-23 16:27:27 +0800

    send message prevent impersonation of sendID

--- group_message_has_read.go
+++ group_message_has_read.go
@@ -69,7 +69,7 @@ func (c *ChatHasRead) markGroupMessageAsRead(callback open_im_sdk_callback.Base,
 		}
 	}
 	if len(allUserMessage) == 0 {
-		common.CheckAnyErrCallback(callback, 201, errors.New("message has been marked read"), operationID)
+		common.CheckAnyErrCallback(callback, 201, errors.New("message has been marked read or sender is yourself"), operationID)
 	}
 
 	for userID, list := range allUserMessage {
