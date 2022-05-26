a9ed9091c06fa8b18aa1d5371128f22e209cc5b8
wenxu12345 <44203734@qq.com>
2022-05-26 15:17:26 +0800

    add hangup callback

--- live_signaling.go
+++ live_signaling.go
@@ -78,7 +78,7 @@ func (s *LiveSignaling) doSignalPush(req *api.SignalReq, operationID string) {
 		log.Info(operationID, "recv signal push ", payload.Accept.String())
 		s.listener.OnInviteeAccepted(utils.StructToJsonString(payload.Accept))
 	case *api.SignalReq_Reject:
-		log.Info(operationID, "recv signal push ", payload.Reject.String())
+		log.Info(operationID, "recv signal push  ", payload.Reject.String())
 		s.listener.OnInviteeRejected(utils.StructToJsonString(payload.Reject))
 	//case *api.SignalReq_Cancel:
 	//	log.Info(operationID, "recv signal push ", payload.Cancel.String())
