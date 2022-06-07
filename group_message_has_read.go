e8e5f05977527821a3b39cd87dcc11f39415e1ab
Gordon <1432970085@qq.com>
2022-06-07 10:59:20 +0800

    db file

--- group_message_has_read.go
+++ group_message_has_read.go
@@ -10,6 +10,7 @@ import (
 	"open_im_sdk/pkg/common"
 	"open_im_sdk/pkg/constant"
 	"open_im_sdk/pkg/db"
+	"open_im_sdk/pkg/db/model_struct"
 	"open_im_sdk/pkg/log"
 	"open_im_sdk/pkg/server_api_params"
 	"open_im_sdk/pkg/utils"
@@ -54,7 +55,7 @@ func (c *ChatHasRead) MarkGroupMessageAsRead(callback open_im_sdk_callback.Base,
 	}()
 }
 func (c *ChatHasRead) markGroupMessageAsRead(callback open_im_sdk_callback.Base, msgIDList MarkGroupMessageAsReadParams, groupID, operationID string) {
-	var localMessage db.LocalChatLog
+	var localMessage model_struct.LocalChatLog
 	allUserMessage := make(map[string][]string, 3)
 	messages, err := c.GetMultipleMessage(msgIDList)
 	common.CheckDBErrCallback(callback, err, operationID)
@@ -110,7 +111,7 @@ func (c *ChatHasRead) DoGroupMsgReadState(groupMsgReadList []*sdk_struct.MsgStru
 		}
 		var msgIdListStatusOK []string
 		for _, v := range msgIdList {
-			t := new(db.LocalChatLog)
+			t := new(model_struct.LocalChatLog)
 			if rd.SendID != c.loginUserID {
 				m, err := c.GetMessage(v)
 				if err != nil {
@@ -214,7 +215,7 @@ func (c *ChatHasRead) internalSendMessage(callback open_im_sdk_callback.Base, s 
 	return &sendMsgResp, nil
 
 }
-func msgStructToLocalChatLog(dst *db.LocalChatLog, src *sdk_struct.MsgStruct) {
+func msgStructToLocalChatLog(dst *model_struct.LocalChatLog, src *sdk_struct.MsgStruct) {
 	copier.Copy(dst, src)
 	if src.SessionType == constant.GroupChatType {
 		dst.RecvID = src.GroupID
